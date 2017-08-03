1eacb1bd79328b6abac883efd031f4436c79bdd6
ushowjack <441332608@qq,com>
2017-08-03 23:54:44 +0800

    none

--- static/script/index01.js
+++ static/script/index01.js
@@ -230,18 +230,4 @@ class ScreenConsole {
 
 }
 
-// window.screenConsole = new ScreenConsole();
-// screenConsole.init();
-// screenConsole.log(1);
-// screenConsole.error(1);
-// screenConsole.info(1);
-// window.a = screenConsole.proxyPress('49+49', () => {
-//     screenConsole.consoleShow();
-// });
-// window.b = screenConsole.proxyPress('50+50', () => {
-//     screenConsole.consoleHide();
-// });
-// window.c = screenConsole.proxyPress('51+51', () => {
-//     screenConsole.clear();
-// });
 export default ScreenConsole;
